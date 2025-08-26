743c707fa91aaf4482b0988d219ebdd286b432ae
Jerome Mondol <jeromemondoll11@gmail.com>
2025-08-27 01:24:38 +0600

    cards done

---
+++ cards.js
@@ -0,0 +1,65 @@
+export const cardData = [
+    {
+        img:"./assets/emergency.png",
+        title: "National Emergency Number",
+        subTitle:"National Emergency",
+        tel:"999",
+        category: "All"
+    },
+    {
+        img:"./assets/police.png",
+        title: "Police Helpline Number",
+        subTitle:"Police",
+        tel:"999",
+        category: "Police"
+    },
+    {
+        img:"./assets/fire-service.png",
+        title: "Fire Service Number",
+        subTitle:"Fire Service",
+        tel:"999",
+        category: "Fire"
+    },
+    {
+        img:"./assets/ambulance.png",
+        title: "Ambulance Service",
+        subTitle:"Ambulance",
+        tel:"1994-999999",
+        category: "Health"
+    },
+    {
+        img:"./assets/emergency.png",
+        title: "Women & Child Helpline",
+        subTitle:"Women & Child Helpline",
+        tel:"109",
+        category: "Help"
+    },
+    {
+        img:"./assets/emergency.png",
+        title: "Anti-Corruption Helpline",
+        subTitle:"Anti-Corruption",
+        tel:"106",
+        category: "Govt"
+    },
+    {
+        img:"./assets/emergency.png",
+        title: "Electricity Helpline",
+        subTitle:"Electricity Outage",
+        tel:"16216",
+        category: "Electricity"
+    },
+    {
+        img:"./assets/emergency.png",
+        title: "Brac Helpline",
+        subTitle:"Brac",
+        tel:"16445",
+        category: "NGO"
+    },
+    {
+        img:"./assets/emergency.png",
+        title: "Bangladesh Railway Helpline ",
+        subTitle:"Bangladesh Railway",
+        tel:"163",
+        category: "Travel"
+    },
+]
---
+++ script.js
@@ -0,0 +1,70 @@
+import { cardData } from './cards.js'
+let cardsParent = document.querySelector('#cards');
+
+for(let i = 0; i < cardData.length; i++) { 
+    let childCard = document.createElement('div');
+    childCard.classList.add('bg-white', 'w-[25%]', 'h-80', 'rounded-lg', 'shadow-sm', 'flex', 'flex-col', 'p-9');
+    let childCardElemImg = document.createElement('img');
+    let childCardElemLove = document.createElement('i');
+    childCardElemLove.classList.add('fa-regular', 'fa-heart', 'text-2xl');
+    let childCardElemImgContainer = document.createElement('div');
+    childCardElemImgContainer.classList.add('flex', 'items-center', 'justify-between');
+    let childCardElemTitle = document.createElement('h1');
+    let childCardElemSubTitle = document.createElement('p');
+    let childCardElemTel = document.createElement('p');
+    let childCardElemCategory = document.createElement('span');
+    let childCardElemBTNContainer = document.createElement('div');
+    childCardElemBTNContainer.classList.add('flex', 'justify-between', 'mt-5');
+    let childCardElemCopyBTN = document.createElement('button');
+    let childCardElemCallBTN = document.createElement('button');
+
+
+    const childCardImg = cardData[i].img;
+    const childCardTitle = cardData[i].title;
+    const childCardSubTitle = cardData[i].subTitle;
+    const childCardTel = cardData[i].tel;
+    const childCardCategory = cardData[i].category;
+
+    
+    childCardElemImg.src = childCardImg;
+    childCardElemImg.classList.add("w-10", "h-10", "bg-red-500/50", "p-2", "rounded-lg");
+
+    childCardElemTitle.innerText = childCardTitle;
+    childCardElemTitle.classList.add('text-xl', 'font-bold', 'mt-3');
+
+    childCardElemSubTitle.innerText = childCardSubTitle;
+    childCardElemSubTitle.classList.add('text-md', 'text-gray-500', 'mt-1');
+
+    childCardElemTel.innerText = childCardTel;
+    childCardElemTel.classList.add('text-2xl', 'mt-3', 'font-bold');
+
+    childCardElemCategory.innerText = childCardCategory;
+    childCardElemCategory.classList.add('bg-gray-500/15', 'w-fit', 'px-3', 'rounded-3xl', 'text-sm');
+
+    childCardElemCopyBTN.innerHTML = '<i class="fa-regular fa-copy mr-2"></i>Copy';
+    childCardElemCopyBTN.classList.add('w-[47%]', 'border-1', 'border-gray-300', 'rounded-sm', 'text-gray-500/80', 'cursor-pointer');
+
+    childCardElemCallBTN.innerText = "Call";
+    childCardElemCallBTN.classList.add('w-[47%]', 'bg-[#00A63E]', 'rounded-sm', 'cursor-pointer', 'text-white');
+
+
+
+
+    cardsParent.appendChild(childCard);
+
+
+    childCard.appendChild(childCardElemImgContainer);
+    childCard.appendChild(childCardElemTitle);
+    childCard.appendChild(childCardElemSubTitle);
+    childCard.appendChild(childCardElemTel);
+    childCard.appendChild(childCardElemCategory);
+    childCard.appendChild(childCardElemBTNContainer);
+
+
+    childCardElemImgContainer.appendChild(childCardElemImg);
+    childCardElemImgContainer.appendChild(childCardElemLove);
+
+    childCardElemBTNContainer.appendChild(childCardElemCopyBTN);
+    childCardElemBTNContainer.appendChild(childCardElemCallBTN);
+
+}
